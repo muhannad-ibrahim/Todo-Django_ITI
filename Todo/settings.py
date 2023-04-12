dd90dc2192582146265b6b8ceb0ca1c955dcf3ee
muhannad ibrahim <muhannad.ibrahim2311@gmail.com>
2023-04-12 17:23:03 +0200

    remove django-secret

--- Todo/settings.py
+++ Todo/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xqdb+ab-uyarg+m&b9z5+pm#0vwtu9yz7x3#_grr%=@t_3p&o*'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
